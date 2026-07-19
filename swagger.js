219bcff57cc9465dd9d30f5a5ba5439f56a5a114
lox4r <loxhand@gmail.com>
2026-07-18 23:01:33 -0500

    a

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: "API for managing games and players"
   },
   host: "games-api-oy2k.onrender.com",
-  schemes: ["http"]
+  schemes: ["https"]
 };
 
 const outputFile = "./swagger-output.json";
